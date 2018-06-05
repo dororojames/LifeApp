b667fc42a10089876b2e4cd473d53a72b8ff2411
Dororo <james7751129@gamil.com>
2018-06-05 21:53:16 +0800

    06/05-21:53 commit

--- LifeApp/Game/GuessGameController.swift
+++ LifeApp/Game/GuessGameController.swift
@@ -10,11 +10,78 @@ import UIKit
 
 class GuessGameController: UIViewController {
 
+    
+    
+    
+    var totalTime = 0
+    var timer = Timer()
+    var GuessNum = ""
+    var compareNum=[0,0,0,0]
+    var answer = [0,0,0,0]
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var TimeLabel: UILabel!
+    func timerStart(){
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateTime), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBOutlet weak var one: UILabel!
+
+    @IBAction func SendGuessNum(_ sender: Any) {
+        GuessNum=input.text!
+        if GuessNum.count<4 || GuessNum.count>4{
+            print("長度不合")
+        }
+        else{
+            
+        let sub1=GuessNum.index(GuessNum.startIndex, offsetBy: 0)
+        let sub2=GuessNum.index(GuessNum.startIndex, offsetBy: 1)
+        let sub3=GuessNum.index(GuessNum.startIndex, offsetBy: 2)
+        let sub4=GuessNum.index(GuessNum.startIndex, offsetBy: 3)
+            if GuessNum[sub1]==GuessNum[sub2]||GuessNum[sub1]==GuessNum[sub3]||GuessNum[sub1]==GuessNum[sub4]||GuessNum[sub3]==GuessNum[sub2]||GuessNum[sub4]==GuessNum[sub2]||GuessNum[sub3]==GuessNum[sub4]{
+                print("error")
+                }
+                else
+                {
+                    var num=Int(GuessNum)!
+                    for i in 0...3 {
+                        compareNum[3-i]=num%10
+                        num=num/10
+                        
+                    }
+                    print(compareNum)
+                }
+            
+            
+            }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        input.keyboardType = .numberPad
+        initial()
+        timerStart()
+       
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func initial(){
+        var nums = Array(0...9)
+        for i in 0...3 {
+            let index = Int(arc4random_uniform(UInt32(nums.count)))
+            answer[i] = nums[index]
+            nums.remove(at: index)
+        }
+        TimeLabel.text = "0:00"
+        totalTime = 0
+    }
+    
+    @objc func updateTime(){
+        totalTime += 1
+        TimeLabel.text = String(totalTime/60) + ":" + String(format: "%02d", totalTime%60)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- LifeApp/Map/MapViewController.swift
+++ LifeApp/Map/MapViewController.swift
@@ -10,48 +10,51 @@ import Foundation
 import UIKit
 import MapKit
 
+class tableCell: UITableViewCell
+{
+    @IBOutlet weak var Name: UILabel!
+    @IBOutlet weak var Address: UILabel!
+}
+
 class MapViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
+        return restlist.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "restaurantCell", for: indexPath)
-        cell.textLabel?.text = list[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "restaurantCell", for: indexPath) as! tableCell
+        cell.Name?.text = restlist[indexPath.row].title
+        cell.Address?.text = restlist[indexPath.row].subtitle
         return cell
     }
     
-    
-    
-    
-    var list = [String]()
+    var restlist = [restaurantAnnotation]()
     @IBOutlet weak var mapView: MKMapView!
     
     @IBAction func roll(for segue: UIStoryboardSegue) {
     }
     @IBAction func unwindToselect(for segue: UIStoryboardSegue) {
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let dis:CLLocationDegrees = 3000
         let ntnuLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(25.006803548528282 , 121.53556901440642)
         mapView.setRegion(MKCoordinateRegionMakeWithDistance(ntnuLocation, dis, dis), animated: true)
-        let ntnuPin = restaurantAnnotation(title: "NTNU", subtitle: "school", coordinate: ntnuLocation)
-        mapView.addAnnotation(ntnuPin)
-        list.append("枋敘")
-        list.append("老袁")
-        list.append("薩莉雅")
+        restlist.append(restaurantAnnotation(title: "NTNU", subtitle: "school", coordinate: CLLocationCoordinate2DMake(25.006803548528282 , 121.53556901440642)))
+        restlist.append(restaurantAnnotation(title: "枋敘", subtitle: "小吃", coordinate: CLLocationCoordinate2DMake(25.004280, 121.536681)))
+        restlist.append(restaurantAnnotation(title: "三角冰冰品", subtitle: "冰店", coordinate: CLLocationCoordinate2DMake(25.004701, 121.538025)))
+        restlist.append(restaurantAnnotation(title: "自由51", subtitle: "炒飯", coordinate: CLLocationCoordinate2DMake(25.007496, 121.537789)))
+        mapView.addAnnotations(restlist)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
